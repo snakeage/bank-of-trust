57256b903ae7a4adcd525a590358508922130de4
Alexander <Snakeage@gmail.com>
2020-04-24 19:36:27 +0300

    new master for show

---
+++ precache-manifest.62085a049497fbbafe0ea55feac8ec3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322e479a33ae0fd104027ab3b265f214",
+    "url": "/bank-of-trust/index.html"
+  },
+  {
+    "revision": "513fe50595c983dc483c",
+    "url": "/bank-of-trust/static/css/main.f0585da4.chunk.css"
+  },
+  {
+    "revision": "c1b4960f580908c90e84",
+    "url": "/bank-of-trust/static/js/2.43218c31.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/bank-of-trust/static/js/2.43218c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "513fe50595c983dc483c",
+    "url": "/bank-of-trust/static/js/main.4e7f8dff.chunk.js"
+  },
+  {
+    "revision": "791749b5ee66162b0d80",
+    "url": "/bank-of-trust/static/js/runtime-main.62ff6f92.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/bank-of-trust/static/media/crown.ed28accb.svg"
+  }
+]);
